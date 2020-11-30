1af3b9e2835eeb6a7467b1a3a82a94cae10e52fd
Nik Bosnyak <niklosb@apple.com>
2020-11-30 11:48:31 -0600

    update template

--- 2020/src/template.py
+++ 2020/src/template.py
@@ -1,18 +1,18 @@
-def part1():
+def part1(data):
     pass
 
 
-def part2():
+def part2(data):
     pass
 
 
-def read_input(filename):
+def read_data(filename):
     with open(filename) as f:
         lines = f.readlines()
     return lines
 
 
 if __name__ == '__main__':
-    data = read_input("./resources/template.txt")
+    data = read_data("./resources/template.txt")
     part1(data)
     part2(data)
